0b50fe49afb0c352e51859d16989e4672cc6e6d0
be_member_1 <xufei@aragoncs.com>
2016-10-08 17:08:43 +0800

    hello my first test

--- dome_app/views.py
+++ dome_app/views.py
@@ -1,7 +1,27 @@
+#coding=utf-8
 from django.shortcuts import render
-from django.http import response
-
-# Create your views here.
-def index(request):
-    a=123
-    return response('hello')
+from django import forms
+from django.shortcuts import render_to_response
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import RequestContext
+from dome_app.models import User
+class UserForm(forms.Form):
+    username=forms.CharField(label='用户名',max_length=100)
+    password=forms.CharField(label='密码',widget=forms.PasswordInput())
+    email=forms.EmailField()
+def register(request):
+    if request.method=='POST':
+        form=UserForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            email=form.cleaned_data['email']
+            user=User()
+            user.username=username
+            user.password=password
+            user.email=email
+            user.save()
+            return render_to_response('success.html',locals())
+    else:
+        form=UserForm()
+    return render_to_response('register.html', locals())
